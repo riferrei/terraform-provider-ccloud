65143e0ec6bb56a39bbfa882f5e333bbe88b07b6
riferrei <riferrei@riferrei.com>
2020-10-14 12:31:00 -0400

    Added API key creation to example

--- examples/kafka.tf
+++ examples/kafka.tf
@@ -9,6 +9,19 @@ resource "ccloud_cluster" "new_cluster" {
   cloud_region = "us-east-1"
 }
 
-output "bootstrap_server" {
+resource "ccloud_apikey" "api_key" {
+  environment_id = data.ccloud_environment.default_env.id
+  cluster_id = ccloud_cluster.new_cluster.id
+}
+
+output "_bootstrap_server" {
   value = ccloud_cluster.new_cluster.cluster_endpoint
+}
+
+output "api_key" {
+  value = ccloud_apikey.api_key.key
+}
+
+output "secret" {
+  value = ccloud_apikey.api_key.secret
 }
